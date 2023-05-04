2754f9ba3c92a9bb1e72d71b74af78957074ac77
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-05-03 20:45:52 -0700

    created function to fetch data and added and event listener to button

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-const advice = document.getElementById('advice');
+let advice = document.getElementById("advice");
+let adviceNumber = document.getElementById('adviceNum');
+const adviceAPI = "https://api.adviceslip.com/advice";
+const adviceBtn = document.getElementById('adviceBtn');
 
-const adviceAPI = 'https://api.adviceslip.com/advice';
+const fetchNewAdviceQuote = () => {
+  return fetch(adviceAPI)
+    .then((res) => res.json())
+    .then((data) => {
+      let randomAdviceCall = data.slip;
+      console.log(randomAdviceCall);
+      advice.innerText = `"${randomAdviceCall.advice}"`;
+      adviceNumber.innerText = randomAdviceCall.id;
+    });
+};
 
-fetch(adviceAPI)
-.then(res => res.json())
-.then(data => {
-    let randomAdviceCall = data.slip;
-    advice.innerHTML = randomAdviceCall.advice;
-})
+adviceBtn.addEventListener('click', fetchNewAdviceQuote);
+
