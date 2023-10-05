dea81e9e0c99a1856b5ea9140f171fea99b12928
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-10-04 21:55:44 -0700

    refactor function to an async function

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ let adviceNumber = document.getElementById('adviceNum');
 const adviceAPI = "https://api.adviceslip.com/advice";
 const adviceBtn = document.getElementById('adviceBtn');
 
-const fetchNewAdviceQuote = () => {
-  return fetch(adviceAPI)
-    .then((res) => res.json())
-    .then((data) => {
-      let randomAdviceCall = data.slip;
-      advice.innerText = `"${randomAdviceCall.advice}"`;
-      adviceNumber.innerText = randomAdviceCall.id;
-    });
+const fetchNewAdviceQuote = async ()  => {
+  const res = await fetch(adviceAPI);
+  const data = await res.json();
+  
+  let randomAdviceCall = data.slip;
+  advice.innerText = `"${randomAdviceCall.advice}"`;
+  adviceNumber.innerText = randomAdviceCall.id;
 };
 
 // onload event to invoke fetch api data to return advice and id on screen load.
